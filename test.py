5d1f36eefb415ce27d911e4f5f8fb096cc7afdcf
Eyantra698Sumanto <jeetsumanto123@gmail.com>
2025-03-10 11:30:13 +0530

    This is a test commit

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+#By Sumanto Kar
 
-
-print("hello git")
-a = 1/7
-a = a+5-3/7 - 32/7 - 1/7
-print("a = ",a)
+print("hello git, welcome to IEOR")
+a = 1/2
+b = 2/3
+a = b+5-3/b - 32*b/7 - 1/7
+print("a = ",a+1)
+print("b = ",a==b)
