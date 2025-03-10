0cba2894c0c3e5766a1d9a2af913453abd40cb25
Saurabhm4ra <mehrasaurabh295>
2025-03-10 14:00:33 +0530

    My changes

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
-
+#By Sumanto Kar
+print("This is the session for github")
+b = 90
+c = 98
+d = b + c - c**2 + 1e9
+print(f"The value of d is {d}")
 print("hello git, I am Nitish")
-
-print("hello git")
-a = 2
-a = a+3-7/7 - 5/7 - 1/7
-print("a = ",a*3)
-
-# name : Mohan Krishna
-
-
+a = 1/8
+a = a+10-3/7 - 32/7 - 1/7
+print("Value of a = ",a)
 
 print("hello git, welcome to IEOR")
 a = 1/2
@@ -16,12 +15,3 @@ b = 2/3
 a = b+5-3/b - 32*b/7 - 1/7
 print("a = ",a+1)
 print("b = ",a==b)
-
-print("first push")
-a=a*5
-b=b+8
-
-print("first git command")
-c= 5+8
-d=c+5
-
